9c379083e74a12f4d4705a88a06a5429bfc7f687
Guiribei <guilherme_f_ribeiro@hotmail.com>
2024-05-10 18:19:18 +0000

    ajust css from python anywhere

--- static/js/login.js
+++ static/js/login.js
@@ -7,3 +7,17 @@ if (toastTrigger) {
     toastBootstrap.show()
   })
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const inputs = document.querySelectorAll('.fieldUserLogin, .fieldUserPasswd');
+    const title = document.querySelector('.mchatTitle');
+
+    inputs.forEach(input => {
+        input.addEventListener('focus', () => {
+            title.classList.add('hidden-title');
+        });
+        input.addEventListener('blur', () => {
+            title.classList.remove('hidden-title');
+        });
+    });
+});
